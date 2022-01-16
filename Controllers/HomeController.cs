d196f02c0e3a2ea1725c3b2f895cde5f00b35ca4
AJ Frank <ybafrank@gmail.com>
2022-01-15 19:40:10 -0500

    Removed Privacy page, fixed project settings to be able to run

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using ModernyzeWebsite.Models;
-using System.Diagnostics;
 
-namespace ModernyzeWebsite.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+namespace ModernyzeWebsite.Controllers; 
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+public class HomeController : Controller {
+    private readonly ILogger<HomeController> _logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public HomeController(ILogger<HomeController> logger) {
+        this._logger = logger;
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Index() {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier});
     }
 }
