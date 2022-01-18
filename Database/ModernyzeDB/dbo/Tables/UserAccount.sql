85cca442b7aef1f980c1f341673ddf2a8bd71cd6
AJ Frank <ybafrank@gmail.com>
2022-01-17 19:30:17 -0500

    Uploaded database project to capture database schema

---
+++ Database/ModernyzeDB/dbo/Tables/UserAccount.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserAccount] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Username]  NVARCHAR (50)  NOT NULL,
+    [FirstName] NVARCHAR (50)  NOT NULL,
+    [LastName]  NVARCHAR (50)  NOT NULL,
+    [Email]     NVARCHAR (50)  NOT NULL,
+    [Password]  NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
