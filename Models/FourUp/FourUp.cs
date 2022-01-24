96d35f3470b1eae243b7a063ecddd666668e81a6
AJ Frank <ybafrank@gmail.com>
2022-01-24 10:05:32 -0500

    Created FourUp table and model
    These will keep record of our published google docs to be mirrored on the website.

---
+++ Models/FourUp/FourUp.cs
@@ -0,0 +1,9 @@
+﻿namespace ModernyzeWebsite.Models.FourUp; 
+
+public class FourUp {
+    public int Id { get; set; }
+
+    public string Source { get; set; }
+
+    public DateTime WeekOf { get; set; }
+}
