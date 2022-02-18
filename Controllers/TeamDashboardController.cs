f3c29c5be300d7c1258f9dc5e2e9237c3f0106f8
AJ Frank <ybafrank@gmail.com>
2022-02-18 08:44:40 -0500

    Removed unnecessary entities from TeamDashboard controller

--- Controllers/TeamDashboardController.cs
+++ Controllers/TeamDashboardController.cs
@@ -1,22 +1,10 @@
-﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using ModernyzeWebsite.Models;
 
 namespace ModernyzeWebsite.Controllers;
 
 public class TeamDashboardController : Controller {
-    private readonly ILogger<TeamDashboardController> _logger;
-
-    public TeamDashboardController(ILogger<TeamDashboardController> logger) {
-        this._logger = logger;
-    }
 
     public IActionResult Index() {
         return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error() {
-        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier});
     }
 }
